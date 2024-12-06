8c77064adc34f8ce3565db54cf0eed7e3207dedb
Davi Melo <davicosta01082008@gmail.com>
2024-12-05 21:46:46 -0300

    fix: add listen and console.log

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,32 @@
-import { Elysia, t } from 'elysia'
 import { PrismaClient } from '@prisma/client'
+import { Elysia, t } from 'elysia'
 
 const prisma = new PrismaClient()
 
-const app = new Elysia().post(
-  '/emails',
-  async ({ set, body }) => {
-    set.status = 201
+const app = new Elysia()
+  .post(
+    '/emails',
+    async ({ set, body }) => {
+      set.status = 201
 
-    const email = await prisma.email.findUnique({
-      where: { email: body.email }
-    })
+      const email = await prisma.email.findUnique({
+        where: { email: body.email },
+      })
 
-    if (email) {
-      return
-    }
+      if (email) {
+        return
+      }
 
-    await prisma.email.create({
-      data: { email: body.email }
-    })
-  },
-  {
-    body: t.Object({
-      email: t.String({ format: 'email' }),
-    }),
-  },
-)
+      await prisma.email.create({
+        data: { email: body.email },
+      })
+    },
+    {
+      body: t.Object({
+        email: t.String({ format: 'email' }),
+      }),
+    },
+  )
+  .listen(3000)
+
+console.log(`Server running at ${app.server?.url}`)
